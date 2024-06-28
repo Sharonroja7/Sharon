3b78acaa38454917177348cd23b8fc6474ac7fc1
Sharonroja Nallamalla <sharonroja.n@apiindia.com>
2024-06-28 18:20:12 +0530

    Sharon's First Commit

---
+++ MyFirstSelinium/src/main/java/First.java
@@ -0,0 +1,19 @@
+public class First {
+
+    String name;
+
+    int a;
+    boolean t;
+    float f;
+    double d;
+
+    public static void main(String[] args) {
+        First ft = new First();
+        System.out.println(ft.a);
+        System.out.println(ft.d);
+        System.out.println(ft.name);
+        System.out.println(ft);
+        System.out.println(ft.f);
+        System.out.println(ft.t);
+    }
+}
